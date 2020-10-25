660986480d53d61a447ccc51217eb40e28826347
Nick Perrin <nick.perrin@mail.utoronto.ca>
2020-10-25 15:43:33 -0400

    Hook up menu assets and get them aligned into place, hook up post-processing package and apply to menu

---
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public Image background;
+    public Image earth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(AnimateBackground());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    IEnumerator AnimateBackground()
+    {
+        yield return null;
+    }
+}
