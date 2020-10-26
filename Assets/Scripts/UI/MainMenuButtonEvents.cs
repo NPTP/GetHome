44691bc2d20fb24fdb6472c98c7d099b48423c8f
Nick Perrin <nick.perrin@mail.utoronto.ca>
2020-10-26 17:54:35 -0400

    Main menu 1st draft + remove Unity splash

---
+++ Assets/Scripts/UI/MainMenuButtonEvents.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MainMenuButtonEvents : MonoBehaviour, IPointerEnterHandler, ISelectHandler
+{
+    [Tooltip("The index should match the top-down order of the buttons starting from 0.")]
+    public int buttonIndex;
+    public event EventHandler<OnButtonEventArgs> OnButtonEvent;
+    public class OnButtonEventArgs : EventArgs
+    {
+        public int buttonIndex;
+        public string eventType;
+    }
+    public Button button;
+
+    void Awake()
+    {
+        button = GetComponent<Button>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (button.interactable)
+        {
+            // Debug.Log(this.gameObject.name + " was highlighted");
+            OnButtonEvent?.Invoke(this, new OnButtonEventArgs
+            {
+                buttonIndex = this.buttonIndex,
+                eventType = "Highlighted"
+            });
+        }
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        if (button.interactable)
+        {
+            // Debug.Log(this.gameObject.name + " was selected");
+            OnButtonEvent?.Invoke(this, new OnButtonEventArgs
+            {
+                buttonIndex = this.buttonIndex,
+                eventType = "Selected"
+            });
+        }
+    }
+}
