b9e8c5bd5dab5d3012cdf7995adbb97b1d42c409
Nick Perrin <nick.perrin@mail.utoronto.ca>
2020-10-26 00:06:42 -0400

    Saving work on ui movement for the night

--- Assets/Scripts/UI/MainMenuManager.cs
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -6,11 +6,27 @@ using UnityEngine.UI;
 public class MainMenuManager : MonoBehaviour
 {
     public Button firstSelectedButton;
+    public GameObject buttons;
+    public float buttonsRotRange = 10f;
 
     // Start is called before the first frame update
     void Start()
     {
         firstSelectedButton.Select();
+    }
+
+    void Update()
+    {
+        Vector3 screenPoint = Input.mousePosition;
+        screenPoint.z = 10.0f; //distance of the plane from the camera
+        Quaternion lookRotation = Quaternion.LookRotation(Camera.main.ScreenToWorldPoint(screenPoint), Vector3.up);
+        Quaternion inverse = Quaternion.Inverse(lookRotation);
+
+        buttons.transform.rotation = Quaternion.Euler(
+            Mathf.Clamp(inverse.x, -buttonsRotRange, buttonsRotRange),
+            Mathf.Clamp(inverse.y, -buttonsRotRange, buttonsRotRange),
+            Mathf.Clamp(inverse.z, -buttonsRotRange, buttonsRotRange)
+        );
     }
 
     public void Quit()
