3e8dffb0e1f565ed025bb6174a7f9ca927791ee1
PDiddyDong <46802062+PDiddyDong@users.noreply.github.com>
2020-11-04 14:04:44 -0500

    blah

---
+++ Assets/Scripts/FixedWallCull.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedWallCull : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.parent.GetComponent<GameObject>().SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider col){
+
+    }
+
+    
+}
