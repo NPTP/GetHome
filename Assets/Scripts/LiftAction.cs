9e75826ae513691c023707a12f4d8aca282d84db
PDiddyDong <46802062+PDiddyDong@users.noreply.github.com>
2020-11-03 20:11:11 -0500

    Added the level and a whole wack of stuff

--- Assets/Scripts/LiftAction.cs
+++ Assets/Scripts/LiftAction.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class LiftAction : MonoBehaviour, IObjectAction
 {
-    bool lifted = false;
+    public bool lifted;
     public void action(){
 
         if(lifted){
-            transform.position+= new Vector3(0,-1,0);
+            transform.position+= new Vector3(0,-2,0);
             lifted = false;
-            
         }else{
-            transform.position+= new Vector3(0,1,0);
+            transform.position += new Vector3(0,2,0);
             lifted = true;
         }
     }
