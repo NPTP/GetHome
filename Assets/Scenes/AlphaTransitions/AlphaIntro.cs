7e573085c78d646b0dee8d429a75ff0d2f5382c8
Nick Perrin <nick.perrin@mail.utoronto.ca>
2020-11-04 12:55:47 -0500

    Make alpha screens, still need sounds plugged in as last step

---
+++ Assets/Scenes/AlphaTransitions/AlphaIntro.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class AlphaIntro : MonoBehaviour
+{
+    int numTexts = 5;
+    Animator[] textAnimators;
+    Animator promptAnimator;
+    Animator transitionAnimator;
+    AudioSource audioSource;
+    bool readyToStart = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Transform textParent = GameObject.Find("TextParent").transform;
+        textAnimators = new Animator[numTexts];
+        for (int i = 0; i < numTexts; i++)
+        {
+            textAnimators[i] = textParent.GetChild(i).gameObject.GetComponent<Animator>();
+        }
+        promptAnimator = GameObject.Find("Prompt").GetComponent<Animator>();
+        transitionAnimator = GameObject.Find("Transition").GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+
+        StartCoroutine(PlayIntro());
+    }
+
+    IEnumerator PlayIntro()
+    {
+        yield return new WaitForSeconds(1f);
+
+        for (int i = 0; i < numTexts; i++)
+        {
+            textAnimators[i].SetTrigger("FadeIn");
+            yield return new WaitForSeconds(2f);
+        }
+
+        yield return new WaitForSeconds(1f);
+
+        promptAnimator.SetTrigger("FadeIn");
+        readyToStart = true;
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && readyToStart)
+        {
+            audioSource.Play();
+            promptAnimator.gameObject.SetActive(false);
+            StartCoroutine(ExitIntro());
+        }
+    }
+
+    IEnumerator ExitIntro()
+    {
+        transitionAnimator.SetTrigger("FadeIn");
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
