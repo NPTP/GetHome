4d3b102c4d2529a0e232002c33c00e8c8524ff01
tylerweston <tylerjohnweston@gmail.com>
2020-11-08 14:12:26 -0500

    Player can't stack on crate

--- Assets/Scripts/BoxStacking.cs
+++ Assets/Scripts/BoxStacking.cs
@@ -9,33 +9,34 @@ public class BoxStacking : MonoBehaviour
     // Start is called before the first frame update
     private void OnCollisionEnter(Collision collision)
     {
-
+        if (collision.gameObject.tag == "Player")
+        {
+            return; // Ignore the player! We don't ever want to stack them on the crate!
+        }
         if (collision.transform.position.y > transform.position.y)
         {
 
             childobject = collision.gameObject.transform;
-            Debug.Log("I'm " + this.gameObject + " and my child is " + collision.gameObject);
-            // Debug.Log("Collision is here!");
-            //origparent = collision.gameObject.transform.parent.gameObject;
-            //collision.gameObject.transform.parent = this.transform;
         }
     }
 
     private void OnCollisionExit(Collision collision)
     {
+        if (collision.gameObject.tag == "Player")
+        {
+            return; // Ignore the player! We don't ever want to unstack them from crates!
+        }
+
         if (collision.transform.position.y > transform.position.y)
         {
-            //collision.gameObject.transform.parent = origparent.transform;
             childobject = null;
         }
     }
 
     public void DoMove(Vector3 move)
     {
-        Debug.Log("I'm in DOMOVE");
         if (childobject)
         {
-            Debug.Log("And I have a child!");
             childobject.position += move;
         }
     }
