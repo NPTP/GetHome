dd72848da021de70a8e4cb2a201341ef924ce4b3
Nick Perrin <nick.perrin@mail.utoronto.ca>
2020-11-10 15:32:23 -0500

    DOTween, dialog system, beginnings of state manager

---
+++ Assets/Scripts/Game/StateManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using UnityEngine;
+
+// State machine for the whole game, based primarily on player state.
+public class StateManager : MonoBehaviour
+{
+    public StateManager.State state;
+    public enum State
+    {
+        Normal,     /* Take normal inputs. */
+        Dialog,     /* Disallow movement/interaction, hit prompts only. */
+        Inert,      /* Block all inputs. Use for e.g. switching characters.  */
+        Paused,     // Not set up yet
+        Crate,      // Not set up yet
+        Flipping    // Not set up yet
+    }
+
+    GameObject selectedCharacter;   /* Use to keep track of player/robot selection. */
+
+    public event EventHandler<StateArgs> OnState;
+    public class StateArgs : EventArgs
+    {
+        public StateManager.State state;
+    }
+
+    void Start()
+    {
+        SetState(State.Normal);
+        selectedCharacter = GameObject.FindWithTag("Player");
+    }
+
+    // This method is for the other managers to tell us what state we're in.
+    public void SetState(StateManager.State newState)
+    {
+        this.state = newState;
+        Debug.Log(">>>>>>>>>> STATE SET TO: " + this.state);
+        OnState?.Invoke(this, new StateArgs { state = newState });
+    }
+
+    public StateManager.State GetState()
+    {
+        return this.state;
+    }
+
+    public void SetSelected(GameObject selected)
+    {
+        selectedCharacter = selected;
+    }
+
+    public GameObject GetSelected()
+    {
+        return selectedCharacter;
+    }
+
+    public void EndInert()
+    {
+        if (this.state == State.Inert)
+            SetState(State.Normal);
+        // Else let the current state be.
+    }
+
+}
---
+++ Assets/Scripts/UI/DialogManager.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using DG.Tweening;
+
+
+public class DialogBox
+{
+    public CanvasGroup canvasGroup;
+    public TMP_Text header;
+    public TMP_Text subtitle1;
+    public TMP_Text subtitle2;
+    public TMP_Text paragraphs;
+    public Image prompt;
+    public Animator animator;
+    float fadeUpTime = 1f;
+    float fadeDownTime = 1f;
+
+    public Tween SetUp()
+    {
+        header.color = Helper.ChangedAlpha(header.color, 0f);
+        subtitle1.color = Helper.ChangedAlpha(header.color, 0f);
+        subtitle2.color = Helper.ChangedAlpha(header.color, 0f);
+        paragraphs.maxVisibleCharacters = 0;
+        prompt.enabled = false;
+        prompt.color = Helper.ChangedAlpha(prompt.color, 0);
+        return canvasGroup.DOFade(1f, fadeUpTime).From(0f);
+    }
+
+    public Tween TearDown()
+    {
+        header.color = Helper.ChangedAlpha(header.color, 0f);
+        subtitle1.color = Helper.ChangedAlpha(header.color, 0f);
+        subtitle2.color = Helper.ChangedAlpha(header.color, 0f);
+        paragraphs.maxVisibleCharacters = 0;
+        prompt.enabled = false;
+        prompt.color = Helper.ChangedAlpha(prompt.color, 0);
+        return canvasGroup.DOFade(0f, fadeDownTime);
+    }
+
+    public void Disable()
+    {
+        header.color = Helper.ChangedAlpha(header.color, 0f);
+        subtitle1.color = Helper.ChangedAlpha(header.color, 0f);
+        subtitle2.color = Helper.ChangedAlpha(header.color, 0f);
+        paragraphs.maxVisibleCharacters = 0;
+        prompt.enabled = false;
+        prompt.color = Helper.ChangedAlpha(prompt.color, 0);
+        canvasGroup.alpha = 0f;
+        animator.SetBool("animate", false);
+    }
+
+    public void AddParagraph(Dialog dialog, int paragraphIndex)
+    {
+        dialog.paragraphs[paragraphIndex] += "\n\n";
+        paragraphs.text += dialog.paragraphs[paragraphIndex];
+    }
+
+    public void ShowPrompt()
+    {
+        prompt.enabled = true;
+        StartPromptWaitAnim();
+    }
+
+    public void HidePrompt()
+    {
+        prompt.color = Helper.ChangedAlpha(prompt.color, 0f);
+        StopPromptWaitAnim();
+        prompt.enabled = false;
+    }
+
+    public void StartPromptWaitAnim()
+    {
+        animator.SetBool("animate", true);
+    }
+
+    public void StopPromptWaitAnim()
+    {
+        animator.SetBool("animate", false);
+    }
+}
+
+
+// Class for encapsulating dialogs sent from other classes to be played back.
+public class Dialog
+{
+    public string header;
+    public string subtitle1;
+    public string subtitle2;
+    public string[] paragraphs;
+}
+
+// The dialog manager handles all dialogs and sends info to the UI to control it.
+// It is called from the Day by any type of trigger, or manual call (e.g. for narration).
+public class DialogManager : MonoBehaviour
+{
+    StateManager stateManager;
+    DialogBox dialogBox;
+    bool dialogNext = false;
+    bool dialogFinished = true;
+    float speed = 0.005f; // Fraction of second delay between characters appearing
+
+    void Start()
+    {
+        stateManager = GameObject.FindObjectOfType<StateManager>();
+
+        dialogBox = new DialogBox();
+        dialogBox.canvasGroup = GameObject.Find("DialogBox").GetComponent<CanvasGroup>();
+        dialogBox.header = GameObject.Find("DialogBoxHeader").GetComponent<TMP_Text>();
+        dialogBox.subtitle1 = GameObject.Find("DialogBoxSubtitle1").GetComponent<TMP_Text>();
+        dialogBox.subtitle2 = GameObject.Find("DialogBoxSubtitle2").GetComponent<TMP_Text>();
+        dialogBox.paragraphs = GameObject.Find("DialogBoxParagraphs").GetComponent<TMP_Text>();
+        dialogBox.prompt = GameObject.Find("DialogBoxPrompt").GetComponent<Image>();
+        dialogBox.animator = GameObject.Find("DialogBoxPrompt").GetComponent<Animator>();
+
+        dialogBox.Disable();
+
+        StartCoroutine(Test());
+    }
+
+    IEnumerator Test()
+    {
+        yield return new WaitForSeconds(2f);
+
+        Dialog d = new Dialog();
+        d.header = "Test header";
+        d.subtitle1 = "Test sub1";
+        d.subtitle2 = "Test sub2";
+        d.paragraphs = new string[] {"Paragraph 1 Paragraph 1 Paragraph 1 Paragraph 1 Paragraph 1 Paragraph 1 Paragraph 1 Paragraph 1 Paragraph 1 ",
+        "Paragraph 2 Paragraph 2 Paragraph 2 Paragraph 2 Paragraph 2 Paragraph 2 Paragraph 2 Paragraph 2 Paragraph 2 Paragraph 2 Paragraph 2 ",
+        "Paragraph 3.",
+        "That's it! (#4)"};
+
+        NewDialog(d);
+    }
+
+    void Update()
+    {
+        if (stateManager.GetState() == StateManager.State.Dialog &&
+            Input.GetButtonDown("Interact"))
+        {
+            dialogNext = true;
+        }
+    }
+
+    // Start a new dialog.
+    public void NewDialog(Dialog dialog)
+    {
+        dialogFinished = false;
+        stateManager.SetState(StateManager.State.Dialog);
+        StartCoroutine(DialogPlay(dialog));
+    }
+
+    public void EndDialog()
+    {
+        if (stateManager.GetState() == StateManager.State.Dialog && !dialogFinished)
+        {
+            dialogBox.TearDown();
+            dialogFinished = true;
+        }
+    }
+
+    IEnumerator DialogPlay(Dialog dialog)
+    {
+        // STEP 1 : Fade box in
+        dialogFinished = false;
+        Tween setup = dialogBox.SetUp();
+        yield return new WaitWhile(() => setup != null && setup.IsPlaying());
+
+        // STEP 2 : Fade in the headers one by one.
+        dialogBox.header.text = dialog.header;
+        dialogBox.header.DOFade(1f, .5f);
+        yield return new WaitForSeconds(.25f);
+        dialogBox.subtitle1.text = dialog.subtitle1;
+        dialogBox.subtitle1.DOFade(1f, .5f);
+        yield return new WaitForSeconds(.25f);
+        dialogBox.subtitle2.text = dialog.subtitle2;
+        dialogBox.subtitle2.DOFade(1f, .5f);
+        yield return new WaitForSeconds(.5f);
+
+        // STEP 3: Show the paragraphs one by one.
+        dialogBox.paragraphs.text = "";
+        int curLength = 0;
+        for (int p = 0; p < dialog.paragraphs.Length; p++)
+        {
+            dialogBox.AddParagraph(dialog, p);
+            for (int i = 0; i <= dialog.paragraphs[p].Length; i++)
+            {
+                dialogBox.paragraphs.maxVisibleCharacters = curLength + i;
+                yield return new WaitForSeconds(speed);
+            }
+            dialogBox.ShowPrompt();
+            dialogNext = false;
+            yield return new WaitUntil(() => dialogNext);
+            dialogBox.HidePrompt();
+            curLength += dialog.paragraphs[p].Length;
+        }
+
+        // STEP 4 : Finish, tear down dialog box, reset state to Normal.
+        dialogBox.TearDown();
+        dialogFinished = true;
+        stateManager.SetState(StateManager.State.Normal);
+    }
+
+    public bool IsDialogFinished()
+    {
+        return dialogFinished;
+    }
+}
