1fc36a2a20b0ba59efd806dabb6fdf3da6ce1ae3
tylerweston <tylerjohnweston@gmail.com>
2020-11-10 16:01:24 -0500

    Box can have multiple items stacked

--- Assets/Scripts/BoxStacking.cs
+++ Assets/Scripts/BoxStacking.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityScript.Steps;
 
 public class BoxStacking : MonoBehaviour
 {
     private GameObject origparent;
-    private Transform childobject;
+    private List<GameObject> childobjects;
     // Start is called before the first frame update
+    private void Start()
+    {
+        childobjects = new List<GameObject> { };
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Player")
@@ -15,8 +20,7 @@ public class BoxStacking : MonoBehaviour
         }
         if (collision.transform.position.y > transform.position.y)
         {
-
-            childobject = collision.gameObject.transform;
+            childobjects.Add(collision.gameObject);
         }
     }
 
@@ -26,18 +30,15 @@ public class BoxStacking : MonoBehaviour
         {
             return; // Ignore the player! We don't ever want to unstack them from crates!
         }
-
-        if (collision.transform.position.y > transform.position.y)
-        {
-            childobject = null;
-        }
+        childobjects.Remove(collision.gameObject);
+        
     }
 
     public void DoMove(Vector3 move)
     {
-        if (childobject)
+        foreach (GameObject childobject in childobjects)
         {
-            childobject.position += move;
+            childobject.transform.position += move;
         }
     }
 }
