dc73b0cb29ccdc8dba7e2ee71df730cdb99358af
Nick Perrin <nick.perrin@mail.utoronto.ca>
2020-11-14 15:39:40 -0500

    Get light fading in/out on occlusion volumes started

--- Assets/Scripts/OcclusionVolume.cs
+++ Assets/Scripts/OcclusionVolume.cs
@@ -1,23 +1,48 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class OcclusionVolume : MonoBehaviour
 {
+    StateManager stateManager;
+
     public LayerMask layerMask;
     BoxCollider boxCollider;
     private List<Collider> liveColliders = new List<Collider>();
     public List<Collider> GetColliders() { return liveColliders; }
     private Collider[] levelColliders;
 
+    // List of tuples with the light and its saved ORIGINAL intensity.
+    private List<Tuple<Light, float>> lightIntensityPairs;
+    float lightFadetime = 0.25f;
+
     bool playerInside = false;
     bool robotInside = false;
 
     void Start()
     {
+        stateManager = FindObjectOfType<StateManager>();
         boxCollider = GetComponent<BoxCollider>();
+        lightIntensityPairs = new List<Tuple<Light, float>>();
+
         GetLevelColliders();
-        HideLevelColliders(); // Start by hiding by default.
+        HideLevelColliders();
+        GetAllLights();
+        HideLights();
+    }
+
+    void GetAllLights()
+    {
+        Light[] allLights = FindObjectsOfType<Light>();
+        foreach (Light l in allLights)
+        {
+            if (this.boxCollider.bounds.Contains(l.transform.position))
+            {
+                lightIntensityPairs.Add(new Tuple<Light, float>(l, l.intensity));
+            }
+        }
     }
 
     void GetLevelColliders()
@@ -30,12 +55,29 @@ public class OcclusionVolume : MonoBehaviour
         );
     }
 
+    void HideLights()
+    {
+        foreach (Tuple<Light, float> t in lightIntensityPairs)
+        {
+            t.Item1.DOIntensity(0f, lightFadetime);
+        }
+    }
+
+    void ShowLights()
+    {
+        foreach (Tuple<Light, float> t in lightIntensityPairs)
+        {
+            t.Item1.DOIntensity(t.Item2, lightFadetime);
+        }
+    }
+
     void HideLevelColliders()
     {
         foreach (Collider collider in levelColliders)
         {
             collider.gameObject.GetComponent<Renderer>().enabled = false;
         }
+
     }
 
     void ShowLevelColliders()
@@ -56,7 +98,8 @@ public class OcclusionVolume : MonoBehaviour
             robotInside = true;
 
         if (playerInside || robotInside)
-            ShowLevelColliders();
+            // ShowLevelColliders();
+            ShowLights();
 
         if (!liveColliders.Contains(other)) { liveColliders.Add(other); }
     }
@@ -71,7 +114,8 @@ public class OcclusionVolume : MonoBehaviour
             robotInside = false;
 
         if (!playerInside && !robotInside)
-            HideLevelColliders();
+            // HideLevelColliders();
+            HideLights();
 
         liveColliders.Remove(other);
     }
