c21b3189febbc5afd610aa64a99b0687290afac8
Nick Perrin <nick.perrin@mail.utoronto.ca>
2020-11-18 01:07:39 -0500

    Match with main to avoid merge conflict

--- Assets/Scripts/Keytrigger.cs
+++ Assets/Scripts/Keytrigger.cs
@@ -8,7 +8,7 @@ public class Keytrigger : MonoBehaviour
 
     public GameObject prompt;
 
-    public bool key = false;
+    public ThirdPersonCharacter key;
 
     bool inTrigger;
     // Start is called before the first frame update
@@ -20,44 +20,32 @@ public class Keytrigger : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (inTrigger)
-        {
+        if(inTrigger){
             //take keypress
-            if (Input.GetButtonDown("Interact") || Input.GetKeyDown(KeyCode.E))
-            {
+            if (Input.GetButtonDown("Interact") || Input.GetKeyDown(KeyCode.E)){
                 MonoBehaviour[] list = toChangeObject.gameObject.GetComponents<MonoBehaviour>();
-                foreach (MonoBehaviour mb in list)
+                foreach(MonoBehaviour mb in list)
                 {
                     if (mb is IObjectAction)
                     {
-                        if (key)
-                        {
-                            IObjectAction actor = (IObjectAction)mb;
-                            actor.action();
-                        }
+                        IObjectAction actor = (IObjectAction)mb;
+                        actor.action();
                     }
-                }
+                 }
             }
         }
     }
 
-    void OnTriggerEnter(Collider player)
-    {
+    void OnTriggerEnter(Collider player){
         //put prompt on screen
-        if (player.name.Equals("Robot"))
+        if(key.HasKey)
             inTrigger = true;
-        prompt.SetActive(true);
-
+            prompt.SetActive(true);
+        
     }
 
-    void OnTriggerExit(Collider player)
-    {
+    void OnTriggerExit(Collider player){
         inTrigger = false;
         prompt.SetActive(false);
-    }
-
-    public void setKey()
-    {
-        key = true;
     }
 }
