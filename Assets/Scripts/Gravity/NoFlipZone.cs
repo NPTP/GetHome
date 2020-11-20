89b0d85377f7df80c32c94b9f3dc88b188a4c9c0
Nick Perrin <nick.perrin@mail.utoronto.ca>
2020-11-20 13:43:07 -0500

    Fix noflip trigger logic for player/bot mix

--- Assets/Scripts/Gravity/NoFlipZone.cs
+++ Assets/Scripts/Gravity/NoFlipZone.cs
@@ -1,14 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 // Note that NoFlipZones are on their own layer which ONLY collides with player & robot - nothing else.
-// So we do not have to check tags on entering triggers, nor worry about other stuff tripping it up.
 public class NoFlipZone : MonoBehaviour
 {
     BoxCollider boxCollider;
-    Transform playerTransform;
-    Transform robotTransform;
+    bool playerInZone = false;
+    bool robotInZone = false;
 
     // Used by the gravity manager to check if it's safe to flip where the character are now.
     [HideInInspector]
@@ -17,27 +14,27 @@ public class NoFlipZone : MonoBehaviour
     void Awake()
     {
         boxCollider = GetComponent<BoxCollider>();
-        playerTransform = GameObject.FindWithTag("Player").transform;
-        robotTransform = GameObject.FindWithTag("robot").transform;
     }
 
     void OnTriggerEnter(Collider other)
     {
         characterInZone = true;
+
+        if (other.tag == "Player") playerInZone = true;
+        if (other.tag == "robot") robotInZone = true;
     }
 
     void OnTriggerExit(Collider other)
     {
-        if (!CheckCharacterInBounds())
-        {
-            characterInZone = false;
-        }
+        if (other.tag == "Player") playerInZone = false;
+        if (other.tag == "robot") robotInZone = false;
+
+        UpdateInZoneStatus();
     }
 
-    // TODO: verify that this is okay, or we might need to check colliders instead of positions.
-    bool CheckCharacterInBounds()
+    void UpdateInZoneStatus()
     {
-        return boxCollider.bounds.Contains(playerTransform.position) || boxCollider.bounds.Contains(robotTransform.position);
+        if (playerInZone || robotInZone) characterInZone = true;
+        else characterInZone = false;
     }
-
 }
