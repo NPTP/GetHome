5854b6d36419def860f225724c5ee43c0294c00b
Nick Perrin <nick.perrin@mail.utoronto.ca>
2020-11-20 17:40:31 -0500

    Add dotween to liftaction

--- Assets/Scripts/LiftAction.cs
+++ Assets/Scripts/LiftAction.cs
@@ -1,27 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class LiftAction : MonoBehaviour, IObjectAction
 {
+    StateManager stateManager;
     public bool lifted;
     public Transform ogParent;
-    public void action(){
 
-        if(lifted){
-            transform.position+= new Vector3(0,-2,0);
+    Vector3 originalPosition;
+
+    void Start()
+    {
+        stateManager = FindObjectOfType<StateManager>();
+        originalPosition = transform.position;
+    }
+
+    public void action()
+    {
+
+        if (lifted)
+        {
+            // transform.position+= new Vector3(0,-2,0);
+            transform.DOMove(originalPosition, 1f).SetEase(Ease.OutCubic);
             lifted = false;
-        }else{
-            transform.position += new Vector3(0,2,0);
+        }
+        else
+        {
+            // transform.position += new Vector3(0,2,0);
+            transform.DOMove(originalPosition + new Vector3(0, 2, 0), 1f).SetEase(Ease.OutCubic);
             lifted = true;
         }
     }
 
-    void OnCollisionEnter(Collision collision){
-        collision.transform.SetParent(transform);
-    }
+    // void OnCollisionEnter(Collision collision)
+    // {
+    //     collision.transform.SetParent(transform);
+    // }
 
-    void OnCollisionExit(Collision collision){
-        collision.transform.parent = ogParent;
-    }
+    // void OnCollisionExit(Collision collision)
+    // {
+    //     collision.transform.parent = ogParent;
+    // }
 }
