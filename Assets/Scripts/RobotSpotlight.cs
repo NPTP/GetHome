c1b5904a0d4610c4fae3bef8731ddf580e5030ac
Nick Perrin <nick.perrin@mail.utoronto.ca>
2020-11-21 18:23:22 -0500

    Change robot prefab to have spotlight

---
+++ Assets/Scripts/RobotSpotlight.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class RobotSpotlight : MonoBehaviour
+{
+    Light spotlight;
+    ThirdPersonUserControl thirdPersonUserControl;
+    RobotBuddy robotBuddy;
+    bool botSelected = false;
+
+    void Start()
+    {
+        spotlight = GetComponent<Light>();
+        thirdPersonUserControl = FindObjectOfType<ThirdPersonUserControl>();
+        thirdPersonUserControl.OnSwitchChar += HandleSwitchChar;
+        robotBuddy = FindObjectOfType<RobotBuddy>();
+    }
+
+    void HandleSwitchChar(object sender, ThirdPersonUserControl.SwitchCharArgs args)
+    {
+        if (args.selected.tag == "robot")
+            botSelected = true;
+        else
+            botSelected = false;
+    }
+
+    void Update()
+    {
+        float inputStrength = Mathf.Abs(Input.GetAxis("Horizontal")) + Mathf.Abs(Input.GetAxis("Vertical"));
+        if (!botSelected || inputStrength > 0)
+            transform.forward = Vector3.Lerp(transform.forward, robotBuddy.spotlightDirection, 20f * Time.deltaTime);
+    }
+}
