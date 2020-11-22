249f53cf3bb0c5daf9e62ea0878befdaafb83a67
tylerweston <tylerjohnweston@gmail.com>
2020-11-22 14:12:03 -0500

    fix vhs pause video

---
+++ Assets/InitVHSVideo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class InitVHSVideo : MonoBehaviour
+{
+    VideoPlayer vp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vp = GetComponent<VideoPlayer>();
+        vp.targetCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
+}
