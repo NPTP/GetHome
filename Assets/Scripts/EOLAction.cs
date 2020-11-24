0eeff7a38322090917bfa89eac349afd8d060598
Nick Perrin <nick.perrin@mail.utoronto.ca>
2020-11-24 13:20:30 -0500

    EOLElevator prefab is ready to be plopped into scenes replacing old elevator

--- Assets/Scripts/EOLAction.cs
+++ Assets/Scripts/EOLAction.cs
@@ -1,13 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+﻿using UnityEngine;
+using DG.Tweening;
 
 public class EOLAction : MonoBehaviour, IObjectAction
 {
+    public Transform elevator = null; // Add this in the inspector.
+    public AudioSource audioSource = null; // Add this in the inspector.
+
+    bool robotInTrigger = false;
+
     public void action()
     {
-        // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        if (elevator != null && audioSource != null)
+        {
+            GameObject.FindWithTag("Player").transform.SetParent(elevator);
+            if (robotInTrigger)
+                GameObject.FindWithTag("robot").transform.SetParent(elevator);
+            audioSource.Play();
+            elevator.DOMoveY(elevator.position.y - 50, 50);
+        }
         FindObjectOfType<SceneLoader>().LoadNextScene();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        robotInTrigger = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        robotInTrigger = false;
     }
 }
