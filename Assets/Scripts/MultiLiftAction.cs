0209fe81759a15e7f2d0171b1d5ba514cd11d283
Nick Perrin <nick.perrin@mail.utoronto.ca>
2020-11-25 13:51:06 -0500

    Final sounds and prep scripts to allow for audiosources (nullchecked)

--- Assets/Scripts/MultiLiftAction.cs
+++ Assets/Scripts/MultiLiftAction.cs
@@ -9,6 +9,7 @@ public class MultiLiftAction : MonoBehaviour, IObjectAction
     public LiftAction[] lifts = new LiftAction[size];
 
     public void action(){
+        GetComponent<AudioSource>()?.Play();
         foreach (LiftAction lift in lifts){
             lift.action();
         }
--- Assets/Scripts/RobotActivationAction.cs
+++ Assets/Scripts/RobotActivationAction.cs
@@ -6,7 +6,9 @@ public class RobotActivationAction : MonoBehaviour, IObjectAction
 {
     // Start is called before the first frame update
     public ThirdPersonUserControl player;
-    public void action(){
+    public void action()
+    {
+        GetComponent<AudioSource>()?.Play();
         player.canSelectBot = true;
     }
 }
