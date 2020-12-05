da25cc6012d16e3defacedb5967433f032e8840e
tylerweston <tylerjohnweston@gmail.com>
2020-12-05 12:24:23 -0500

    Light effects

---
+++ Assets/Scripts/LightEffects/PulseLight.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PulseLight : MonoBehaviour
+{
+    Light thisLight;
+    // we'll make the range pulse so it doesn't interfere with the intensity that is controlled
+    // by the occlusion volumes
+    float lightOriginalRange;
+    public float pulseAmount = 3;
+    public float pulseSpeed = 2;
+    
+    void Start()
+    {
+        thisLight = GetComponent<Light>();
+        lightOriginalRange = thisLight.range;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        thisLight.range = lightOriginalRange + pulseAmount * Mathf.Sin(Time.time * pulseSpeed);
+    }
+}
