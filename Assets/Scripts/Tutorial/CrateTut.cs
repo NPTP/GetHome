8b9f47f9bf247de56bdbd8141b4ff555273f19d1
tylerweston <tylerjohnweston@gmail.com>
2020-12-05 10:42:04 -0500

    Crate tutorial

---
+++ Assets/Scripts/Tutorial/CrateTut.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CrateTut : MonoBehaviour
+{
+    GravityManager gravityManager;
+    TutorialScreen crateTutorialScreen;
+
+    void Start()
+    {
+        gravityManager = FindObjectOfType<GravityManager>();
+        crateTutorialScreen = GameObject.Find("CrateTutorial").GetComponent<TutorialScreen>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            gravityManager.StopLookingOnPickup("LookUpTutorial");
+            crateTutorialScreen.StartTutorial();
+            Destroy(this.gameObject);
+        }
+    }
+}
