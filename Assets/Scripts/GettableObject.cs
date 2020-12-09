44a6a97c7f39396cb055dc8a05d99ae190b4025b
tylerweston <tylerjohnweston@gmail.com>
2020-12-09 10:02:55 -0500

    level 1-2, keycard

--- Assets/Scripts/GettableObject.cs
+++ Assets/Scripts/GettableObject.cs
@@ -5,21 +5,40 @@ using UnityEngine;
 public class GettableObject : MonoBehaviour
 {
     private ThirdPersonCharacter m_Character; // A reference to the ThirdPersonCharacter
+    private Renderer thisrenderer;
     public string ItemName;
+    private AudioSource audios;
+    // check to make sure we only play the keycard sound once
+    private bool hasPlayed;
     private void Start()
     {
         // get the third person character ( this should never be null due to require component )
         m_Character = GameObject.FindGameObjectWithTag("Player").GetComponent<ThirdPersonCharacter>();
+        audios = GetComponent<AudioSource>();
+        thisrenderer = GetComponent<Renderer>();
+        hasPlayed = false;
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.tag == "Player")
         {
+            thisrenderer.enabled = false;
             // TODO: player has now collected something!
             m_Character.GetItem(ItemName);
-            Destroy(gameObject);
-
+            StartCoroutine("WaitDestroy");
         }
+    }
+
+    IEnumerator WaitDestroy()
+    {
+        float tlength = audios.clip.length;
+        if (!hasPlayed)
+        {
+            audios.Play();
+            hasPlayed = true;
+        }
+        yield return new WaitForSeconds(tlength);
+        Destroy(gameObject);
     }
 }
