98217d8e91460ae9e32ce8e3ae23be222237b32a
tylerweston <tylerjohnweston@gmail.com>
2020-12-09 10:33:27 -0500

    projector stuck around after getting key

--- Assets/Scripts/GettableObject.cs
+++ Assets/Scripts/GettableObject.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GettableObject : MonoBehaviour
 {
     private ThirdPersonCharacter m_Character; // A reference to the ThirdPersonCharacter
+    private GameObject childFx;
     private Renderer thisrenderer;
     public string ItemName;
     private AudioSource audios;
@@ -14,6 +15,7 @@ public class GettableObject : MonoBehaviour
     {
         // get the third person character ( this should never be null due to require component )
         m_Character = GameObject.FindGameObjectWithTag("Player").GetComponent<ThirdPersonCharacter>();
+        childFx = this.transform.GetChild(0).gameObject;
         audios = GetComponent<AudioSource>();
         thisrenderer = GetComponent<Renderer>();
         hasPlayed = false;
@@ -24,6 +26,7 @@ public class GettableObject : MonoBehaviour
         if (collision.gameObject.tag == "Player")
         {
             thisrenderer.enabled = false;
+            childFx.SetActive(false);
             // TODO: player has now collected something!
             m_Character.GetItem(ItemName);
             StartCoroutine("WaitDestroy");
