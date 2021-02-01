09b78ff7ab9d6fc53b898f28e016fe560288a063
Nick Perrin <nick.perrin@mail.utoronto.ca>
2021-02-01 10:49:14 -0500

    add new decorations on 3-2

---
+++ Assets/Scripts/Level32/CRTWall.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CRTWall : MonoBehaviour
+{
+
+    [SerializeField] Texture[] staticFrames;
+    Renderer rdr;
+    int index = 0;
+    bool animating = true;
+    float frameTime = 0.042f; // 24 fps
+
+    void Start()
+    {
+        rdr = GetComponent<Renderer>();
+        StartCoroutine(Animation());
+    }
+
+    IEnumerator Animation()
+    {
+        while(animating)
+        {
+            if (index >= staticFrames.Length)
+                index = 0;
+            
+            rdr.material.SetTexture ("_EmissionMap", staticFrames[index]);
+            index++;
+
+            float elapsed = 0;
+            while (elapsed < frameTime)
+            {
+                elapsed += Time.deltaTime;
+                yield return null;
+            }
+        }
+    }
+}
